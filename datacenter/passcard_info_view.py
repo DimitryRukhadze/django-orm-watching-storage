1cba0e395acb2150250894319a2b67058a982443
DimitryRukhadze <lokiinflames@gmail.com>
2022-05-29 14:55:18 +0300

    Update passcard_info_view.py

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 
 def passcard_info_view(request, passcode):
     passcard = Passcard.objects.filter(passcode=passcode)[0]
+    passcard_visits = Visit.objects.filter(passcard=passcard.pk)
 
     this_passcard_visits = [
         {
@@ -12,7 +13,7 @@ def passcard_info_view(request, passcode):
             'duration': visit.format_duration(),
             'is_strange': visit.is_visit_long()
         }
-        for visit in Visit.objects.filter(passcard=passcard.pk)
+        for visit in passcard_visits
     ]
 
     context = {
